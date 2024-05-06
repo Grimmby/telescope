e4011bd9403ec2d018f34153e7755c7afa75e8ea
Camille C <camille@latelier-conceptionweb.com>
2024-05-06 18:24:09 +0200

    chores: install swiper js dependency

--- web/app/themes/telescope/resources/scripts/app.js
+++ web/app/themes/telescope/resources/scripts/app.js
@@ -1,6 +1,7 @@
 import domReady from '@roots/sage/client/dom-ready';
 import { animationsScript } from '@scripts/theme/animations';
 import { headerScript } from '@scripts/theme/header';
+import 'swiper/css';
 
 /**
  * Application entrypoint
