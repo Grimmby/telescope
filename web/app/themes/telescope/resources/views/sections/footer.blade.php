d56267325adb4cae14c5edb303e5782bf546bdbf
Camille C <camille@latelier-conceptionweb.com>
2024-05-06 18:05:07 +0200

    feat: add site footer

--- web/app/themes/telescope/resources/views/sections/footer.blade.php
+++ web/app/themes/telescope/resources/views/sections/footer.blade.php
@@ -1,3 +1,31 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="w-full py-12 lg:py-[5.188rem] px-5 sm:px-8 border-t-2 border-lower bg-white">
+
+	<div class="flex flex-wrap items-start justify-between gap-y-12 gap-x-6 w-full max-w-[1280px] mx-auto transition-all duration-500 r-opacity reveal-delay-200 scroll-reveal">
+
+		<a href="{{ home_url('/') }}" title="{{ _x( 'Telescope homepage', 'Accueil Telescope', 'theme-telescope' ) }}"
+			class="js__header-logo shrink-0 block w-[12.5rem]">
+			<x-svg.logo class="size-full object-contain" />
+		</a>
+
+		<div class="flex flex-col md:flex-row justify-start lg:justify-end items-start gap-y-12 gap-x-20 lg:gap-x-28 w-full lg:w-auto">
+			@php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'footer_navigation',
+					'menu_class' => 'flex flex-col md:flex-row gap-y-12 gap-x-20 lg:gap-x-28',
+					'container'  => false,
+					'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+				)
+			);
+			@endphp
+			<div>
+				<p class="mb-6 font-secondary text-[1.125rem] font-semibold">
+					{{ _x( 'Follow us', 'Suivez-nous', 'theme-telescope' ) }}
+				</p>
+				<x-social-media />
+			</div>
+		</div>
+
+	</div>
+
 </footer>
